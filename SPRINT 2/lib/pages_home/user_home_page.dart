5e7caee6228d543b8b12c12c80157ef6441a54a6
MarceloReisxz <marceloestevesreis@gmail.com>
2023-09-20 23:06:43 -0300

    Sprint2

---
+++ SPRINT 2/lib/pages_home/user_home_page.dart
@@ -0,0 +1,78 @@
+import 'package:BskCenter/registro/login/registrar_page.dart';
+import 'package:flutter/material.dart';
+import '../menu/menu_page.dart';
+
+
+class UserHomePage extends StatelessWidget {
+  const UserHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Basketball Center'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(203, 0, 0, 0),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle_rounded),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: const Drawer(child: MenuPage()),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 30.0),
+                child: Icon(
+                  Icons.error_outline,
+                  size: 80, // Tamanho do ícone
+                  color: Colors.red, // Cor do ícone
+                ),
+              ),
+
+              const SizedBox(height: 10), // Espaço entre o ícone e o texto
+              const Text(
+                'Time favorito ainda não escolhido!',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => RegistrarPage(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(100, 50),
+                    backgroundColor: const Color.fromARGB(203, 0, 0, 0),
+                  ),
+                  child: const Text(
+                    'Registrar',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
